ca5dc5124188c6bdd36753575f4507d0c5684813
root <root@example.com>
2018-04-05 21:10:27 +0530

    add respose php

--- php/reg.php
+++ php/reg.php
@@ -1,9 +1,56 @@
-<form action="signup.html" method="POST">
-    <label>User name <input type="text" name="username"></label>
-    <label>Password <input type="password" name="password"></label>
-    <label>Email <input type="email" name="email"></label>
-    <label>Phone <input type="number" name="phone"></label>
-    <input type="submit" value="Login">
-    <a href="index.html"><button type="button">Back</button></a>
-</form>
-<p id="error" style="color:red; text-align:center;">error text</p>
+<?php
+    $message="";
+    $response='<form>
+            <label>User name <input type="text" name="username"></label>
+            <label>Password <input type="password" name="password"></label>';
+    if($_COOKIE["auth"]=="true"){
+        echo "user already logged in";
+        exit(0);
+    }
+    else if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $db = mysqli_connect("localhost","gallery","password","gallery");
+        if(mysqli_connect_errno()){
+            printf("Connection failed %s<br/>",mysqli_error());
+            exit();
+        }
+        
+        $query = "SELECT * from userdata";
+        $result = mysqli_query($db,$query);
+        
+        // extract post req and search for username
+        $back=search_db($result,$_POST["username"]);
+        
+        //if found set cookie and set message
+        if($back&&$back["password"]==$_POST["password"]){
+            setcookie("auth","true",time()+86400);
+            setcookie("username",$back["username"],time()+86400);
+            $message="successfully logged in";
+        }
+        else{
+            $message="user not found / password invalid";
+        }
+    }
+    else if($_SERVER["REQUEST_METHOD"]=="GET"){
+        if($_GET["q"]=="signup"){
+            $response.='<label>Email <input type="email" name="email"></label>
+                <label>Phone <input type="number" name="phone"></label>';
+        }
+    }
+    $response.='<input type="submit" value="Login">
+        <a href="index.html"><button type="button">Back</button></a>
+        </form>
+        <p id="error" style="color:red; text-align:center;">'."$message</p>";
+    echo $response;
+    function search_db($result,$username){
+        while($row = mysqli_fetch_assoc($result)){
+            if($username==$row["username"]){
+                return $row;
+            }
+        }
+        return false;
+    } 
+?>
+
+
+
+ 
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
 var login = document.getElementById("login");
 var signup = document.getElementById("signup");
-
 var regBack=document.querySelector(".reg-back");
 
 var xhttp = new XMLHttpRequest();
-
 
 login.addEventListener("click",function(){
     regBack.style.display="flex";
@@ -12,6 +10,7 @@ login.addEventListener("click",function(){
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.querySelector(".form-wrap").innerHTML = this.responseText;
+            validator();
         }
     };
     xhttp.open("GET", "php\\reg.php", true);
@@ -24,8 +23,54 @@ signup.addEventListener("click",function(){
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.querySelector(".form-wrap").innerHTML = this.responseText;
+            validator(true);
         }
     };
-    xhttp.open("GET", "php\\reg.php", true);
+    xhttp.open("GET", "php\\reg.php?q=signup", true);
     xhttp.send();
 });
+
+function validator(signup){
+    var input = document.querySelectorAll("input");
+    var form = document.querySelector("form");
+
+    var errorText = document.getElementById("error");
+
+    var emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var phoneRegExp = /[0-9]{10}/;
+
+    errorText.style.display="none";
+
+    form.addEventListener("submit",function(event){
+        for(i=0;i<input.length;i++){
+            if(input[i].name=="username") var username = input[i].value;
+            if(input[i].name=="password") var password = input[i].value;
+            if(input[i].name=="email") var email = input[i].value;
+            if(input[i].name=="phone") var phone = input[i].value;
+        }
+        var message="";
+        if(username=="") message+="username required</br>";
+        if(password=="") message+="password required</br>";
+        if(signup){
+            if(!emailRegExp.test(email)) message+="email invalid</br>";
+            if(!phoneRegExp.test(phone)) message+="phone number invalid";
+        }
+        if(message!=""){ 
+            event.preventDefault();
+            errorText.innerHTML=message;
+            errorText.style.display="block";
+        }
+        else if(signup){
+            event.preventDefault();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.querySelector(".form-wrap").innerHTML = this.responseText;
+                    validator(true);
+                }
+            };
+            xhttp.open("POST", "php\\reg.php?q=signup", true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.send("username="+username+"&password="+password+"&email="+email+"&phone="+phone);
+        }
+    });
+}
